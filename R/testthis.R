1021bd2cdef1932f0260166d4e1b053d6e1c231c
stefan <unabledispute@gmail.com>
2017-01-28 12:57:43 +0100

    updated doc

--- R/testthis.R
+++ R/testthis.R
@@ -7,8 +7,29 @@
 #'
 #' Testthis provides several tools to make unit testing in R slightly more
 #' confortable. It is designed to be used in conjunction with \code{testthat}
-#' package. I designed this package after my own workflow so many of the
+#' package. I modelled this package after my own workflow so many of the
 #' functions it provides expect that you organize your tests in a certain way.
+#'
+#' @section Rstudio addins (requires Rstudio):
+#'
+#' This package contains several simple Rstudio addins. The motiviation
+#' behind defining those function as Rstudio addins is that you can easily
+#' assign hotkeys to them in Rstudio (\code{Tools/Modify Keyboard Shortcuts})
+#'
+#' Testthis assumes that for each \code{anyfile.R} file in your package`s
+#' \code{R/} folder you have a testfile \code{/test_anyfile.R} in your test dir
+#' (usually \code{/tests/testthat/}). If you want your testfile to be named
+#' differently, you can add the comment \code{#* @testfile someotherfile}
+#' anywhere in \code{anyfile.R}
+#'
+#' \describe{
+#'   \item{test_this}{Tests the currently open file}
+#'   \item{lest_this}{(("Load and test") devtools::load_all and test the currently open files}
+#'   \item{open_tests}{If the current filename is \code{currentfile.R}, this
+#'                     this opens \code{/tests/testthat/test_currentfile.R} in
+#'                     the editor. If the file does not exists, it is created
+#'                     with a minimal testing infrastructure skeleton.}
+#' }
 #'
 #' @section Test coverage checklist:
 #'
@@ -32,26 +53,8 @@
 #'   \item{get_exported_functions}{List all tested functions a package}
 #' }
 #'
-#' @section Rstudio addins (requires Rstudio):
-#'
-#' This package contains several simple Rstudio addins. The motiviation
-#' behind defining those function as Rstudio addins is that you can easily
-#' assign hotkeys to them in Rstudio (\code{Tools/Modify Keyboard Shortcuts})
-#'
-#' Testthis assumes that for each \code{anyfile.R} file in your package`s
-#' \code{R/} folder you have a testfile \code{/test_anyfile.R} in your test dir
-#' (usually \code{/tests/testthat/}). If you want your testfile to be named
-#' differently, you can add the comment \code{#* @testfile someotherfile}
-#' anywhere in \code{anyfile.R}
-#'
-#' \describe{
-#'   \item{test_this}{Tests the currently open file}
-#'   \item{lest_this}{(("Load and test") devtools::load_all and test the currently open files}
-#'   \item{open_tests}{If the current filename is \code{currentfile.R}, this
-#'                     this opens \code{/tests/testthat/test_currentfile.R} in
-#'                     the editor. If the file does not exists, it is created
-#'                     with a minimal testing infrastructure skeleton.}
-#' }
+
+
 #'
 #' @docType package
 NULL
