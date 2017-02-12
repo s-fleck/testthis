4aef47ac36fac9b396ab883300182bf51eb0ed85
stefan <unabledispute@gmail.com>
2017-02-12 15:28:02 +0100

    Added documentation, changed function name to be distinct from devtools::test

--- R/test.R
+++ R/test.R
@@ -1,15 +1,22 @@
-test <- function(pkg = '.', skip = TRUE, ...){
-  if(skip){
-    fltr <- list_test_files(
-      pkg,
-      full_names = FALSE,
-      skip = TRUE
-    )
-    fltr <- gsub("^test-?", "", fltr)
-    fltr <- gsub("\\.[rR]", "", fltr)
+#' Execute all \pkg{test_that} tests in a package.
+#'
+#' This is a wrapper for \code{\link[devtools]{test}} that skips \code{test_*.R}
+#' files that contain the testthis tag "\code{#' @skip}".
+#'
+#' See the devtools documentation for further info or
+#' \code{vignette("testthis")} for infos on testthis tags.
+#'
+#' @inheritParams devtools::test
+#'
+#' @export
+test_with_skip <- function(pkg = '.', ...){
+  fltr <- list_test_files(
+    pkg,
+    full_names = FALSE,
+    skip = TRUE
+  )
+  fltr <- gsub("^test-?", "", fltr)
+  fltr <- gsub("\\.[rR]", "", fltr)
 
-    devtools::test(filter = fltr, ...)
-  } else {
-    devtools::test(pkg = pkg, ...)
-  }
+  devtools::test(filter = fltr, ...)
 }
