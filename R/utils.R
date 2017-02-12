7ebba677f0e2cbfbb4192edc7520b18b25044dd6
stefan <unabledispute@gmail.com>
2017-02-12 11:05:59 +0100

    list_test_files now supports @skip tags. reorg.

--- R/utils.R
+++
@@ -1,9 +0,0 @@
-list_test_files <- function(pkg, full_names = TRUE){
-  pkg     <- devtools::as.package(pkg)
-  tpath   <- file.path(pkg$path, 'tests', 'testthat')
-  res     <- list.files(
-    tpath,
-    full.names = full_names)
-
-  res[grepl('.*\\.R$', res, ignore.case = TRUE)]
-}
