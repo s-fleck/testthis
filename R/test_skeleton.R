8d54a80fdad8d0f95fed5e0f8cf4f46a18906dfc
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-05-20 08:27:34 +0200

    test_skeleton now honors #' @testfile tags

---
+++ R/test_skeleton.R
@@ -0,0 +1,89 @@
+#' Create a test skeleton file  for the currently open .R file
+#'
+#' If the file currently open in the Rstudio editor is called \code{my_function.R},
+#' this creates the file \file{/tests/testthat/test_my_function.R} and fills it
+#' with a basic test skeleton.
+#'
+#' @section Side effects:
+#'   Creates a file.
+#'
+#' @param fname optional: Targt R script file to open. If empty the file
+#'   currently open in the editor will be used.
+#' @param open Should the test file be opend after it is created?
+#'
+#' @export
+#'
+test_skeleton <- function(
+  fname,
+  open = TRUE,
+  sep = options('testthis.sep')
+){
+  if(!requireNamespace("rstudioapi")){
+    stop('This function is designed to be used from within Rstudio')
+  }
+
+
+  if(missing(fname)){
+    fname <- get_testfile_name(sep = sep)
+  }
+
+  if(file.exists(fname)){
+    message(sprintf('* File alread exists: %s', fname))
+  } else {
+    message(sprintf('* Creating `%s`', fname))
+    title_name <- stringi::stri_sub(
+      tools::file_path_sans_ext(basename(fname)), 6
+    )
+
+    lines <- paste0(
+      sprintf('context("%s")', title_name),
+      "\n\n\n",
+      sprintf('test_that("%s works as expected", {\n\n\n})', title_name)
+    )
+
+    writeLines(lines, fname)
+  }
+
+  if(open){
+    rstudioapi::navigateToFile(fname)
+  }
+}
+
+
+
+
+# utils -------------------------------------------------------------------
+
+get_pkg_testfile_names_from_tags <- function(pkg = '.'){
+  infiles <- list_rdir_files(pkg)
+
+  res <- lapply(infiles, get_taglist) %>%
+    lapply(get_tag, 'testfile') %>%
+    setNames(infiles) %>%
+    unlist()
+
+  data.frame(
+    rfile = names(res),
+    tfile = as.character(res),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+get_testfile_name_from_tag <- function(infile){
+  assert_that(is.scalar(infile))
+
+  res <- infile %>%
+    get_taglist() %>%
+    get_tag('testfile')
+
+  if(is.null(res)) return(NULL)
+
+  data.frame(
+    rfile = infile,
+    tfile = paste0(res, '.R'),
+    stringsAsFactors = FALSE
+  )
+}
+
