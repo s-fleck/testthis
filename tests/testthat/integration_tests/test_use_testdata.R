8293a9062d05dbe2e50844e36e76cfbb20ced114
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-09-29 14:36:17 +0200

    ensure cleanup of temporary files

--- tests/testthat/integration_tests/test_use_testdata.R
+++ tests/testthat/integration_tests/test_use_testdata.R
@@ -2,12 +2,17 @@ context("use_testdata")
 
 
 test_that("use_testdata works as expected", {
+  package_state <- list.files(".", recursive = TRUE)
+
   expect_message(use_testdata(iris), "Saving to")
   expect_true(file.exists(rprojroot::find_testthat_root_file("testdata", "iris.rds")))
 
   tdat <- read_testdata("iris.rds")
   expect_identical(iris, tdat)
 
-  unlink("testdata/iris.rds")
-  expect_false(file.exists("testdata/iris.rds"))
+  unlink(rprojroot::find_testthat_root_file("testdata", "iris.rds"))
+  expect_false(file.exists(rprojroot::find_testthat_root_file("testdata", "iris.rds")))
+
+  # Check if tests did not modify package
+  expect_identical(list.files(".", recursive = TRUE), package_state)
 })
