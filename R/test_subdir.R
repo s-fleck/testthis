29611559a14b3991e38e16f95d8912e3b8874ce0
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-10-07 08:57:47 +0200

    now returns a testthat_results object instead of NULL
    updated doc

--- R/test_subdir.R
+++ R/test_subdir.R
@@ -1,35 +1,47 @@
-#' Execute all test_that tests in a subdir.
+#' Use test subdirectories
 #'
 #' This is a simple wrapper for [devtools::test()], but rather than running
-#' the tests in `inst/tests/` or `tests/testthat`, it runs the tests in a
-#' specified subdirectory of that folder.
+#' the tests in \file{inst/tests/} or \file{tests/testthat}, it runs the tests
+#' in a subdirectory of that folder. For creating such subdirectories,
+#' please also see [`use_test_subdir()`].
+#'
+#'
+#' @section Test subdirectory presets:
 #'
 #' Three preset test subdirs are defined at the moment:
 #'
-#' \tabular{ll}{
-#'   `test_integration()` \tab Integration tests, also called component tests.
+#' \describe{
+#'   \item{`test_integration()`}{Integration tests, also called component tests.
 #'     Put tests here that test if several functions / parts of your program
-#'     work together as expected \cr
-#'   `test_acceptance()` \tab Acceptance tests. This is the highest levels of
+#'     work together as expected. You can create the relevant subdir
+#'     \file{testthat/integration_tests/} with [`use_integration_tests()`].
+#'
+#'     }
+#'   \item{`test_acceptance()`}{Acceptance tests. This is the highest levels of
 #'     tests. Put tests here that verifies if your package fulfills the
-#'     goals/requirements you set out to achieve with your package were met. \cr
-#'   `test_manual()` \tab Manual tests. Put tests here that produce output
+#'     goals/requirements you set out to achieve with your package were met.
+#'     You can create the relevant subdir \file{testthat/acceptance_tests/}
+#'     with [`use_acceptance_tests()`].
+#'     }
+#'   \item{`test_manual()`}{Manual tests. Put tests here that produce output
 #'     that has to be manually verified, such as: console output, pdf files,
 #'     plots. It is recommended you collect the output files of such tests in
-#'     'tests/testthat/test_out'. \cr
+#'     \file{tests/testthat/testout}. You can create the relevant subdir
+#'     with \file{testthat/manual_tests/} with [`use_manual_tests()`].}
 #' }
 #'
-#' The above functions assume the respective test files are in
-#' `testthat/integration_tests`, `testthat/acceptance_tests` or
-#' `testthat/manual_tests`
+#' You can modify the default paths for manual, acceptance and integration tests
+#' by setting the respective `options()`, but it is recommended to create your
+#' own test subdirs instead.
 #'
 #' @inheritParams devtools::test
 #' @template base_path
 #' @param subdir subdir of `inst/tests/` or `tests/testthat` that contains the
 #'   tests
 #' @param ... passed on to `devtools::test()`
+#' @seealso [`use_test_subdir()`]
 #'
-#' @return `NULL` (invisibly)
+#' @return A [testthat_results] object (invisibly)
 #'
 #' @export
 test_subdir <- function(subdir, base_path = '.', ...){
@@ -49,12 +61,10 @@ test_subdir <- function(subdir, base_path = '.', ...){
   }
 
 
-  testthat::with_mock(
+  invisible(testthat::with_mock(
     `devtools:::find_test_dir` = find_test_dir_mock,
     devtools::test(pkg = base_path, ...)
-  )
-
-  invisible()
+  ))
 }
 
 
